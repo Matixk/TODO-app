cf2e39ecea8ac442acc1a287e124243302610703
matix <matix@DESKTOP-2P37HKM.local>
2019-05-09 11:36:07 +0200

    Add new task form to index site

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
     <title>TODO app</title>
 </head>
 <body>
+    <h1>Task form</h1>
+    <form id="newTaskForm">
+        Title:
+        <input name="title"
+               type="text" placeholder="Title"
+               minlength="10" maxlength="255" required />
+
+        Description:
+        <textarea name="description"
+                  placeholder="Description"
+                  maxlength="255"></textarea>
+
+        <input type="submit" value="Add" id="createTaskBtn" />
+    </form>
 </body>
 </html>
